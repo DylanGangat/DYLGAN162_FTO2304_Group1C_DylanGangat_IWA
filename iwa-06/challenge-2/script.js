63daeed448f40947ed7fa728c2e2016eb2879667
dylangangat <dylan.gangat@gmail.com>
2023-06-27 17:22:21 +0200

    Update challenge 2 script.js

--- iwa-06/challenge-2/script.js
+++ iwa-06/challenge-2/script.js
@@ -1,16 +1,16 @@
-const tax = 8;
 const rent = 400;
+const tax = "8%";
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-let hourOfDay = "00";
-let minuteOfDay = "00";
+const hourOfDay = 00;
+const minuteOfDay = 00;
 
 // Only change below this line
 
-if (hourOfDay !== "undefined" && minuteOfDay !== "undefined" && hourOfDay === "00" && minuteOfDay === "00") {
-  const taxAsDecimal = tax / 100;
-  const startingAfterTax = salary - (salary * taxAsDecimal);
+if (hourOfDay !== "undefined" && minuteOfDay !== "undefined" && hourOfDay === parseInt("00") && minuteOfDay === parseInt("00")) {
+  const taxAsDecimal = Number(tax.slice(0, 1)) / 100;
+  const startingAfterTax = salary - salary * taxAsDecimal;
   const balance = startingAfterTax - transport - food - rent;
   console.log("R" + balance.toFixed(2));
 }
