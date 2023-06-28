bda931bc91966c47d2c642094bd78b9d261addb2
dylangangat <dylan.gangat@gmail.com>
2023-06-28 09:16:26 +0200

    Update challenge-1 script.js of iwa-06

--- iwa-06/challenge-1/script.js
+++ iwa-06/challenge-1/script.js
@@ -3,8 +3,8 @@ const secondaryPhone = "0219131568";
 
 // Only change below this line
 
-const primaryValid = parseInt(primaryPhone) === "number"; // NaN is a number
-const secondaryValid = typeof parseInt(secondaryPhone) === "number";
+const primaryValid = Number(primaryPhone) === "number"; // NaN is a number
+const secondaryValid = typeof Number(secondaryPhone) === "number";
 
 console.log("Primary phone is valid numerical string:", primaryValid);
 console.log("Secondary phone is valid numerical string:", secondaryValid);
