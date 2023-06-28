1ca06fb7ecf7f540d6810d9639487781a6d35d2c
dylangangat <dylan.gangat@gmail.com>
2023-06-28 17:03:21 +0200

    Complete challenge 2 of iwa-07

--- iwa-07/script.js
+++ iwa-07/script.js
@@ -2,3 +2,10 @@
 
 const value = 3;
 console.log(value + 4 + value);
+
+// Challenge 2
+
+const nickName = "Timmy";
+const firstName = "Timothy";
+
+console.log(`Good Morning, ${nickName || firstName}!`);
