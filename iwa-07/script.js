1ff3dd09382c07aa107bb579d3b6cb1f7895ea28
dylangangat <dylan.gangat@gmail.com>
2023-06-29 20:15:55 +0200

    Complete challenge 3 of iwa-07

--- iwa-07/script.js
+++ iwa-07/script.js
@@ -9,3 +9,40 @@ const nickName = "Timmy";
 const firstName = "Timothy";
 
 console.log(`Good Morning, ${nickName || firstName}!`);
+
+// Challenge 3
+
+const leoName = "Leo";
+const leoSurname = "Musvaire     ";
+const leoBalance = "-9394";
+
+const sarahName = "Sarah    ";
+const sarahSurname = "Kleinhans";
+const sarahBalance = "-4582.21000111";
+
+const divider = "----------------------------------";
+
+// Only change below this line
+
+const leoCorrectBalance = Math.abs(leoBalance).toFixed(2);
+const sarahCorrectBalance = Math.abs(sarahBalance).toFixed(2);
+
+const owed = `R ${parseFloat(leoCorrectBalance) + parseFloat(sarahCorrectBalance)}`;
+
+const owedNumberFormatted = `${owed.slice(0, 4)} ${owed.slice(4)}`;
+
+const leo = `${leoName} ${leoSurname.trim()} (Owed: R ${leoCorrectBalance})`;
+
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${sarahCorrectBalance})`;
+
+const total = `Total amount owed:`;
+
+const result = `
+${leo}
+${sarah}
+
+${divider}
+  ${total} ${owedNumberFormatted}
+${divider}`;
+
+console.log(result);
