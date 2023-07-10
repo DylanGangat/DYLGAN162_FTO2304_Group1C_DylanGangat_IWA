efd8c955827175faedb56a0e75b5b3ebaaabf71a
dylangangat <dylan.gangat@gmail.com>
2023-07-10 22:44:06 +0200

    Complete challenge 1 of iwa-12

--- iwa-12/script.js
+++ iwa-12/script.js
@@ -31,14 +31,12 @@ const book1Status = book1.querySelector(".status");
 const book1ReserveBtn = book1.querySelector(".reserve");
 const book1CheckOutBtn = book1.querySelector(".checkout");
 const book1CheckInBtn = book1.querySelector(".checkin");
-console.log(book1, book1Status, book1ReserveBtn, book1CheckOutBtn, book1CheckInBtn);
 
 const book2 = document.getElementById("book2");
 const book2Status = book2.querySelector(".status");
 const book2ReserveBtn = book2.querySelector(".reserve");
 const book2CheckOutBtn = book2.querySelector(".checkout");
 const book2CheckInBtn = book2.querySelector(".checkin");
-console.log(book2, book2Status, book2ReserveBtn, book2CheckOutBtn, book2CheckInBtn);
 
 const book3 = document.getElementById("book3");
 const book3Status = book3.querySelector(".status");
@@ -49,21 +47,20 @@ console.log(book3, book3Status, book3ReserveBtn, book3CheckOutBtn, book3CheckInB
 
 book1CheckInBtn.style.color = "";
 book1Status.style.color = STATUS_MAP.overdue.color;
-
-// reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+book1ReserveBtn.disabled = STATUS_MAP.overdue.canReserve === false ? true : false;
+book1CheckOutBtn.disabled = STATUS_MAP.overdue.canCheckout === false ? true : false;
+book1CheckInBtn.disabled = STATUS_MAP.overdue.canCheckIn === false ? true : false;
 
 book2CheckInBtn.style.color = "";
 book2Status.style.color = STATUS_MAP.reserved.color;
+book2ReserveBtn.disabled = STATUS_MAP.reserved.canReserve === false ? true : false;
+book2CheckOutBtn.disabled = STATUS_MAP.reserved.canCheckout === false ? true : false;
+book2CheckInBtn.disabled = STATUS_MAP.reserved.canCheckIn === false ? true : false;
 
-// reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
 
 book3CheckInBtn.style.color = "";
 book3Status.style.color = STATUS_MAP.shelf.color;
+book3ReserveBtn.disabled = STATUS_MAP.shelf.canReserve === false ? true : false;
+book3CheckOutBtn.disabled = STATUS_MAP.shelf.canCheckout === false ? true : false;
+book3CheckInBtn.disabled = STATUS_MAP.shelf.canCheckIn === false ? true : false;
 
-// reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
