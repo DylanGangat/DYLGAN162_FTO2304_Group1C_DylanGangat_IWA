4feb94b26e125f6f2de6ee7cd25a00f62b2985fd
DylanGangat <dylan.gangat@gmail.com>
2023-07-13 11:23:06 +0200

    Remove uneccessary comments in script.js of iwa-11

--- iwa-11/script.js
+++ iwa-11/script.js
@@ -1,32 +1,3 @@
-// const item = document.querySelector("[data-order-state='new']");
-// item.foo = "bar";
-
-// console.log(item)
-// item.dataset.orderState = "old";
-// console.log(item.hasAttribute("data-order-state"))
-// console.log(item.getAttribute("data-order-state"));
-// console.log(item.setAttribute("data-order-state", "young"))
-// console.log(item.removeAttribute("data-order-state"))
-
-// // console.log(item.attributes)
-// const names = document.querySelectorAll(".name");
-// const otherNames = document.getElementsByClassName("name");
-// console.log(names, otherNames);
-
-// console.log(document.documentElement.parentNode);
-
-// console.log(document.body);
-// console.dir(document.body);
-
-// document.body.textContent = "<b>Dylan</b>";
-// document.body.innerHTML = "<h1>Dylan</h1>";
-
-// const element = document.querySelector("[data-widget-name]");
-// console.log(element)
-// console.log(element.dataset.widgetName = "bob")
-// element.setAttribute('data-widget-name', 'Dylan')
-// console.log(element);
-
 const root1 = document.querySelector('[data-key="order1"]');
 const biscuits1 = root1.querySelector(".biscuits");
 const donuts1 = root1.querySelector(".donuts");
