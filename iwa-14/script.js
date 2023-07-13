a3e2c929f9b3da436aa9681b92202898364746d6
DylanGangat <dylan.gangat@gmail.com>
2023-07-13 19:01:27 +0200

    Complete challenge 2 of iwa-14

--- iwa-14/script.js
+++ iwa-14/script.js
@@ -38,3 +38,49 @@ function showHobby() {
 }
 
 showHobby();
+
+// Challenge 2
+
+function add() {
+  // add is bound to parent object example1/2
+  return this.internal.a + this.internal.b;
+}
+
+function multiply(added) {
+  // mutiply is bound to parent object example1/2
+  return added * this.internal.c;
+}
+
+function internal() {
+  // internal is bound to parent object example1/2
+  const added = this.add();
+  const answer = this.multiply(added);
+  console.log(answer);
+}
+
+// Not allowed to change below this
+
+const example1 = {
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+const example2 = {
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+example1.calculate();
+example2.calculate();
